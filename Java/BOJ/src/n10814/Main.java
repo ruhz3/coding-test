b4bee4f398b20f50ea65a1c8c75a09834c4f0343
rhz <96fbgudwn@naver.com>
2022-01-27 04:20:03 +0900

    Update JAVA_BOJ_1874 10814 1978

--- Java/BOJ/src/n10814/Main.java
+++ Java/BOJ/src/n10814/Main.java
@@ -1,5 +1,39 @@
 package n10814;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.StringTokenizer;
 
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		HashMap<Integer, ArrayList<String>> hashMap = new HashMap<Integer, ArrayList<String>>();
+		BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int age;
+		String name;
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			age = Integer.parseInt(st.nextToken());
+			name = st.nextToken();
+			if(hashMap.containsKey(age)) {
+				hashMap.get(age).add(name);
+			} else {
+				ArrayList<String> list = new ArrayList<String>();
+				list.add(name);
+				hashMap.put(age, list);
+			}
+		}
+		Iterator<Integer> iter = hashMap.keySet().iterator();
+		while(iter.hasNext()) {
+			int key = iter.next();
+			ArrayList<String> obj = hashMap.get(key);
+			for(int i = 0; i < obj.size(); i++) {
+				System.out.println(key + " " + obj.get(i));
+			}
+		}
+	}
 }
--- Java/BOJ/src/n1978/Main.java
+++ Java/BOJ/src/n1978/Main.java
@@ -1,5 +1,38 @@
 package n1978;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+	public static boolean check(int num) {
+		// 1은 소수가 아니다!
+		if(num == 1) {
+			return false;
+		}
+		// 소수는 1과 자기 자신 외에는 어떤 수도 약수로 갖지 않는다.
+		for(int i = 2; i < num; i++) {
+			if(num % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// 00. 입력을 받는다.
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int num, count = 0;
+		for(int i = 0; i < N; i++) {
+			num = Integer.parseInt(st.nextToken());
+			// 01. 소수라면 count한다. 
+			if(check(num)) {
+				count++;
+			}
+		}
+		// 02. count를 출력한다. 
+		System.out.println(count);
+	}
 }
