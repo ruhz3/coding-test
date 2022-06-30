22e9312ae27558c17f32f08ab4319e0cc2c439a7
류형주 <96fbgudwn@naver.com>
2022-06-30 19:00:57 +0900

    Update

--- Java/BOJ/src/n11403_경로_찾기/Main.java
+++ Java/BOJ/src/n11403_경로_찾기/Main.java
@@ -10,15 +10,28 @@ public class Main {
 	static int N;
 	
 	public static void main(String[] args) throws IOException{
+		// 00. 입력한다.
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		matrix = new boolean[N][N];
 		for(int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			for(int j = 0; j < N; j++) {
-				matrix[i][j] = Integer.parseInt(st.nextToken()) == 1;
-			}
+			for(int j = 0; j < N; j++)
+				matrix[i][j] = st.nextToken().equals("1");
 		}
-		// 플로이드 워샬!
+		// 01. 플로이드 워셜로 연결 여부를 조사한다.
+		for(int k = 0; k < N; k++) {
+			for(int i = 0; i < N; i++)
+				for(int j = 0; j < N; j++)
+					matrix[i][j] = matrix[i][j] || (matrix[i][k] && matrix[k][j]);
+		}
+		// 02. 결과를 출력한다.
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++)
+				sb.append(matrix[i][j] ? 1 : 0).append(" ");
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
 	}
 }
