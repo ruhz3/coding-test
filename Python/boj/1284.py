d2f828b7adfa9cd0a1fd1fd7944b8875718d8775
ruhz <96fbgudwn@naver.com>
2024-01-22 00:42:32 +0900

    2024/01/21

--- Python/boj/1284.py
+++ Python/boj/1284.py
@@ -1,20 +1,24 @@
 from sys import stdin
 
 
-result = ""
-
-while (n := int(stdin.readline())) != 0:
+def solution(num: int) -> int:
     word_width = 1
-    while n > 0:
-        last_num = n % 10
+    while num > 0:
+        last_num = num % 10
         if last_num == 0:
             word_width += 5
         elif last_num == 1:
             word_width += 3
         else:
             word_width += 4
-        n = int(n/10)
+        num = int(num/10)
 
-    result += f"{word_width}\n"
+    return word_width
 
-print(result)
+
+if __name__ == "__main__":
+    result = ""
+    while (n := int(stdin.readline())) != 0:
+        result += f'{solution(n)}\n'
+
+    print(result)
